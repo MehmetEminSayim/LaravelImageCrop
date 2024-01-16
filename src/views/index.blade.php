7c2dfffd37c7c292fec164dc24027738bc34a6de
Mehmet Emin Sayım <55194309+MehmetEminSayim@users.noreply.github.com>
2024-01-16 15:06:51 +0300

    update:style

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,3 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
+
 <style>
     .modal-dialog {
         max-width: 100%;
@@ -13,20 +16,74 @@
         background-color: #f7f7f7;
         overflow: hidden;
     }
+
+    .drop-container {
+        position: relative;
+        display: flex;
+        gap: 10px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        padding: 20px;
+        border-radius: 10px;
+        border: 2px dashed #555;
+        color: #444;
+        cursor: pointer;
+        transition: background .2s ease-in-out, border .2s ease-in-out;
+    }
+
+    .drop-container:hover,
+    .drop-container.drag-active {
+        background: #eee;
+        border-color: #111;
+    }
+
+    .drop-container:hover .drop-title,
+    .drop-container.drag-active .drop-title {
+        color: #222;
+    }
+
+    .drop-title {
+        color: #444;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        transition: color .2s ease-in-out;
+    }
+
+    input[type=file] {
+        width: 350px;
+        max-width: 100%;
+        color: #444;
+        padding: 5px;
+        background: #fff;
+        border-radius: 10px;
+        border: 1px solid #555;
+    }
+
+    input[type=file]::file-selector-button {
+        margin-right: 20px;
+        border: none;
+        background: #084cdf;
+        padding: 10px 20px;
+        border-radius: 10px;
+        color: #fff;
+        cursor: pointer;
+        transition: background .2s ease-in-out;
+    }
+
+    input[type=file]::file-selector-button:hover {
+        background: #0d45a5;
+    }
 </style>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 mt-5">
-            <h3 class="text-center">Upload and Crop Image</h3>
-            <form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <input type="file" name="image" id="image" accept="image/*">
-                </div>
-            </form>
-        </div>
-    </div>
+<div class="row">
+    <label for="images" class="drop-container" id="dropcontainer">
+        <span class="drop-title">Drop files here</span>
+        or
+        <input type="file" name="image" id="image" accept="image/*">
+    </label>
 </div>
 
 <div class="modal fade" id="cropImageModal" tabindex="-1" aria-labelledby="cropImageModalLabel" aria-hidden="true">
@@ -47,17 +104,6 @@
             </div>
         </div>
     </div>
-
-
-</div>
-
-<div class="row">
-    <div class="col-md-12 mt-5">
-        <h5 class="text-center">Cropped Image</h5>
-        <div class="d-flex justify-content-center">
-            <img id="croppedImage" src="#" alt="Cropped image" style="display: none;">
-        </div>
-    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -66,9 +112,36 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js"></script>
 <script>
     $(document).ready(function() {
+        const dropContainer = document.getElementById("dropcontainer")
+        const fileInput = document.getElementById("image")
+
+        dropContainer.addEventListener("dragover", (e) => {
+            // prevent default to allow drop
+            e.preventDefault()
+        }, false)
+
+        dropContainer.addEventListener("dragenter", () => {
+            dropContainer.classList.add("drag-active")
+        })
+
+        dropContainer.addEventListener("dragleave", () => {
+            dropContainer.classList.remove("drag-active")
+        })
+
+        dropContainer.addEventListener("drop", (e) => {
+            e.preventDefault()
+            dropContainer.classList.remove("drag-active")
+            fileInput.files = e.dataTransfer.files;
+            $("#image").trigger("change")
+        })
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+
         let cropper;
         let croppedImageDataURL;
 
+        // Initialize the Cropper.js instance when the modal is shown
         $('#cropImageModal').on('shown.bs.modal', function() {
             cropper = new Cropper($('#imageToCrop')[0], {
                 aspectRatio: 1 / 1,
@@ -77,11 +150,13 @@
             });
         });
 
+        // Destroy the Cropper.js instance when the modal is hidden
         $('#cropImageModal').on('hidden.bs.modal', function() {
             cropper.destroy();
             cropper = null;
         });
 
+        // Show the image cropping modal when an image is selected
         $('#image').on('change', function(event) {
             const file = event.target.files[0];
             const fileReader = new FileReader();
@@ -94,14 +169,17 @@
             fileReader.readAsDataURL(file);
         });
 
+        // Handle the "Crop and Upload" button click
         $('#cropAndUpload').on('click', function() {
             croppedImageDataURL = cropper.getCroppedCanvas().toDataURL();
             uploadCroppedImage();
             $('#cropImageModal').modal('hide');
         });
 
+        // Upload the cropped image to the server
         function uploadCroppedImage() {
             let  csrfToken = "{{ csrf_token() }}";;
+
             const formData = new FormData();
             formData.append('_token', csrfToken);
             formData.append('image', dataURLtoFile(croppedImageDataURL, 'cropped-image.png'));
@@ -110,11 +188,12 @@
                 if (res.data.status === 'success') {
                     $('#croppedImage').attr('src', '{{ env('APP_UPLOADS_URL') }}/' + res.data.filename);
                     $('#croppedImage').show();
-                    console.log(res.data);
                 }
             })
+
         }
 
+        // Helper function to convert a data URL to a File object
         function dataURLtoFile(dataURL, filename) {
             const arr = dataURL.split(',');
             const mime = arr[0].match(/:(.*?);/)[1];
@@ -130,3 +209,6 @@
         }
     });
 </script>
+
+</body>
+</html>
